f7f77f69cd2ec852cb4e190129a93b304a055723
Kamil <kamil.borkow@wp.pl>
2024-02-08 18:50:44 +0100

    Dodano laby 5 SO

--- SO/lab5_so/kk_klient.c
+++ SO/lab5_so/kk_klient.c
@@ -1,44 +1,44 @@
-#include <stdio.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <stdio.h> 
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
 
-struct buffer {
-	long typW;
-	int mesg;
-} message;
+struct buffer { 
+	long typW; 
+	int mesg; 
+} message; 
 
-int main(int argc, char *argv[])
-{
-	key_t key;
-	int msgid;
+int main(int argc, char *argv[]) 
+{ 
+	key_t key; 
+	int msgid; 
 	int n = atoi(argv[1]);//int n jest wprowadzane przy wywolaniu programu, ilosc liczb
 	int i;
-
+	
 	key = ftok("progfile", 65); //generacja klucza
 
 	msgid = msgget(key, 0666 | IPC_CREAT); //podpiecie sie pod kk
-	message.typW = 1; //1 = M_DANE,2 = M_KONIEC, 3 = M_WYNIK
+	message.typW = 1; //1 = M_DANE,2 = M_KONIEC, 3 = M_WYNIK 
 
 	for(i=1;i<=n;i++)
 	{
 		if(i!=n)
 		{
-			printf("Podaj %d liczbe: ",i);
+			printf("Podaj %d liczbe: ",i); 
 			scanf("%d", &message.mesg);
-			msgsnd(msgid, &message, sizeof(message), 0); //wysylanie wiadomosci
+			msgsnd(msgid, &message, sizeof(message), 0); //wysylanie wiadomosci			
 		}
 		else
 		{
-			message.typW = 2;
-			printf("Podaj %d liczbe: ",i);
+			message.typW = 2; 
+			printf("Podaj %d liczbe: ",i); 
 			scanf("%d", &message.mesg);
-			msgsnd(msgid, &message, sizeof(message), 0); //wysylanie wiadomosci
+			msgsnd(msgid, &message, sizeof(message), 0); //wysylanie wiadomosci					
 		}
 	}
 
 	msgrcv(msgid, &message, sizeof(message), 0, 0);
-	printf("Odczytano komunikat M_WYNIK o wartosci: %d \n", message.mesg);
-
+	printf("Odczytano komunikat M_WYNIK o wartosci: %d \n", message.mesg); 
+	
 	msgctl(msgid, IPC_RMID, NULL); //niszczenie kk na koniec programu
-	return 0;
-}
+	return 0; 
+} 
--- SO/lab5_so/kk_serwer.c
+++ SO/lab5_so/kk_serwer.c
@@ -1,45 +1,45 @@
-#include <stdio.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <stdio.h> 
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
 
-struct buffer {
-	long typW;
-	int mesg;
-} message;
+struct buffer { 
+	long typW; 
+	int mesg; 
+} message; 
 
-int main()
-{
-	key_t key;
-	int msgid;
+int main() 
+{ 
+	key_t key; 
+	int msgid; 
 
 	key = ftok("progfile", 65); //generacja klucza
 
 	msgid = msgget(key, 0666 | IPC_CREAT); //podpiecie sie pod kk
 	int pom=1,s=0;
-
+	
 	while(pom)
 	{
 		// msgrcv - odebranie wiadomosci
 		msgrcv(msgid, &message, sizeof(message), 0, 0); //przedostatni argument oznacza jakie typy wiadomosci sa czytane, 0 - wszystkie
-
+		
 		if(message.typW==1)
 		{
-			printf("Odczytano komunikat M_DANE o wartosci: %d \n", message.mesg);
-			s = s+message.mesg;
+			printf("Odczytano komunikat M_DANE o wartosci: %d \n", message.mesg); 
+			s = s+message.mesg;			
 		}
 		if(message.typW==2)
 		{
-			printf("Odczytano komunikat M_KONIEC o wartosci: %d \n", message.mesg);
-			s = s+message.mesg;
-			pom=0;
+			printf("Odczytano komunikat M_KONIEC o wartosci: %d \n", message.mesg); 
+			s = s+message.mesg;	
+			pom=0;			
 		}
 	}
 	message.typW = 3;
 	message.mesg = s;
-	msgsnd(msgid, &message, sizeof(message), 0); //wysylanie wiadomosci
+	msgsnd(msgid, &message, sizeof(message), 0); //wysylanie wiadomosci	
 
-	printf("Wyslano komunikat M_WYNIK: %d \n", s);
+	printf("Wyslano komunikat M_WYNIK: %d \n", s); 
 
 	msgctl(msgid, IPC_RMID, NULL); //niszczenie kk na koniec programu
-	return 0;
-}
+	return 0; 
+} 
