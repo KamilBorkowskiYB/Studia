4741b26c9c94dde95588caee3d21e1dbc5d394e6
Kamil <kamil.borkow@wp.pl>
2024-03-29 10:09:54 +0100

    Rearranged folders for fourth semester

---
+++ SEM_3/BD/projekt/temp/WCY22IY1Z_07.sql
@@ -0,0 +1,6 @@
+@C:\Users\Kamil\Desktop\temp\create_projekt.ddl
+@C:\Users\Kamil\Desktop\temp\populate_projekt.sql
+
+@C:\Users\Kamil\Desktop\temp\drop_projekt.ddl
+
+INSERT INTO nazwy_firm (id_nazwa_firmy, nazwa) VALUES (seq_naz_firm.nextval, 'JanuszPol');
