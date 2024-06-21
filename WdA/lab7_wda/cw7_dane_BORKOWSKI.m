88ad5278652c1f11b7746da38d6a5f28131fee3e
Kamil <kamil.borkow@wp.pl>
2024-06-21 14:03:27 +0200

    Added last WdA programs

---
+++ WdA/lab7_wda/cw7_dane_BORKOWSKI.m
@@ -0,0 +1,39 @@
+%Kamil Borkowski WCY22IY1S1 22.05.2024
+%wariant 14
+%dane:
+%C1 = 10
+%C2 = 5
+%R1 = 1
+%R2 = 2
+%q0 = 1
+%p0 = 2 = x20
+%x10 = 3
+%u = 2
+%e = 0.1 * p0
+C1 = 10
+C2 = 5
+R1 = 1
+R2 = 2
+p0 = 2
+e = 0.1 * p0
+x20 = 2
+x10 = 3
+u = 2
+
+a0=1/(R1 *C1)
+a1=1/(R1*C2)
+a2=1/(R2*C2)
+
+b1=1/C1
+
+
+A=[-a0 a0; a1 -a1-a2]
+B=[b1; 0]
+C=[0 1]
+D=[0]
+
+plot(tout, yout)
+legend('x1(t)[m]','x2(t)[m]','q(t)[m^3/s]','e(t)[m]','p(t)[m]');
+ylabel("Poziom wody[m]")
+xlabel("Czas[s]")
+ginput(2)
