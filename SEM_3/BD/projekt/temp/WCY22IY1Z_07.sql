61085289cd963359d07d4439f41310bcdcc57c22
Kamil <kamil.borkow@wp.pl>
2024-06-24 11:16:53 +0200

    Archived unnecessary files

--- SEM_3/BD/projekt/temp/WCY22IY1Z_07.sql
+++
@@ -1,6 +0,0 @@
-@C:\Users\Kamil\Desktop\temp\create_projekt.ddl
-@C:\Users\Kamil\Desktop\temp\populate_projekt.sql
-
-@C:\Users\Kamil\Desktop\temp\drop_projekt.ddl
-
-INSERT INTO nazwy_firm (id_nazwa_firmy, nazwa) VALUES (seq_naz_firm.nextval, 'JanuszPol');
